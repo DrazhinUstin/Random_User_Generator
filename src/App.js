5a89522916ef11ebdedb1ea6ace3c421327c9857
DrazhinUstin <zmagar91@gmail.com>
2022-01-22 02:20:30 +0300

    Set getting a random user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import icons from './icons';
 
 const App = () => {
+    const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState({});
+    const [gender, setGender] = useState('all');
+
+    const getRandomUser = async () => {
+        const url = `https://randomuser.me/api/`;
+        const fullUrl = `${url}${gender !== 'all' ? `?gender=${gender}` : ''}`;
+        setLoading(true);
+        try {
+            const response = await fetch(fullUrl);
+            const data = await response.json();
+            const {
+                name: { first, last },
+                dob: { age },
+                email,
+                phone,
+                location: { street },
+                picture: { large: image },
+            } = data.results[0];
+            const name = `${first} ${last}`;
+            const adress = `${street.number} ${street.name}`;
+            setUser({ name, age, adress, email, phone, image });
+        } catch (error) {
+            console.log(error);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getRandomUser();
+    }, []);
+
     return (
         <div className='wrapper'>
-            <img
-                src='https://randomuser.me/api/portraits/men/28.jpg'
-                alt='random-user'
-                className='user-image'
-            />
+            <img src={user.image} alt='random-user' className='user-image' />
             <article className='user-info'>
                 <p>My street is</p>
                 <h2>465 Mcgowen St</h2>
@@ -24,13 +52,22 @@ const App = () => {
                 })}
             </div>
             <form className='generate-user-form'>
-                <select id='gender' name='gender'>
+                <select
+                    id='gender'
+                    name='gender'
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                >
                     <option value='all'>all genders</option>
                     <option value='male'>male</option>
-                    <option value='feemale'>feemale</option>
+                    <option value='female'>female</option>
                 </select>
-                <button type='button' className='btn'>
-                    generate
+                <button
+                    type='button'
+                    className={`btn ${loading ? 'active' : ''}`}
+                    onClick={getRandomUser}
+                >
+                    {loading ? 'loading...' : 'generate'}
                 </button>
             </form>
         </div>
